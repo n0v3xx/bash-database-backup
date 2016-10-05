7bcb97f1b2c893962292b5422ba5b0df8ac93711
m.reschke <manuel.reschke90@gmail.com>
2016-10-05 17:29:32 +0200

    Add output messages Fix remove old files command

--- db-backup.sh
+++ db-backup.sh
@@ -23,11 +23,17 @@ compressLevel=9
 umask 177
 
 # Dump database into SQL file
+echo "Start DB Backup"
 mysqldump --user=$user --password=$password --host=$host $db_name > $backup_path/$db_name-$date.sql
+echo "Backup complete"
 
 # compress the SQL file
 cd $backup_path
+echo "Comperess the sql file"
 gzip -f -$compressLevel $db_name-$date.sql
 
-# Delete files older than 30 days
-find $backup_path/* -name *.sql -mtime +30 -exec rm {} \;
+# Delete files older than 30 days (only .sql.gz files)
+echo "Delete old .sql.gz files"
+find $backup_path/* -name '*.sql.gz' -mtime +30 -exec rm {} \;
+
+echo "Run complete."
